eac7b404e4bb0118bb770ab9afa77386185cea22
kkew3 <kps6326@hotmail.com>
2022-08-18 18:34:12 +0800

    Added more options.

--- solveeq.py
+++ solveeq.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 import math
-import sys
+import argparse
 
 import numpy as np
 
@@ -38,22 +38,38 @@ def proc_basis(b: list[float]) -> list[int]:
     return [x.numerator for x in b]
 
 
-def solveeq(string):
+def solveeq(string: str, N: int, allow_decimal: bool):
     A = parseeq.form_coef_mat(parseeq.parseeq(parseeq.preprocesseq(string)))
     p = np.arange(A.shape[0])
     q = np.arange(A.shape[1])
     rank = ext.gaussian_elimination(A, p, q)
     B = nullspace_basis(A, p, q, rank)
-    Q = ext.find_opt_basis_transform(B, 1000000)
+    Q = ext.find_opt_basis_transform(B, N)
     B = np.matmul(Q, B.T).T
     B = [B[:, j].tolist() for j in range(B.shape[1])]
-    B = [proc_basis(b) for b in B]
+    if not allow_decimal:
+        B = [proc_basis(b) for b in B]
     return B
 
 
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--decimal', action='store_true', help='allow decimal coefficient')
+    parser.add_argument(
+        '-N',
+        type=int,
+        default=10000,
+        help=('try N times before stopping finding legal basis; default '
+              '%(default)s'))
+    parser.add_argument('eq', nargs='+', help='the equation')
+    return parser
+
+
 def main():
-    line = ' '.join(sys.argv[1:])
-    sols = solveeq(line)
+    args = make_parser().parse_args()
+    line = ' '.join(args.eq)
+    sols = solveeq(line, args.N, args.decimal)
     for row in sols:
         print(*row)
 
